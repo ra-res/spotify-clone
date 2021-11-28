ce3f5a99b32497728a8347070cecb4a05584620b
nifty <purtan.rares@yahoo.com>
2021-11-28 17:27:07 +0000

    Add middleware to restrict access, configure JWT logic

---
+++ lib/strings.ts
@@ -0,0 +1,3 @@
+const spotifyGreen = '#1DB954'
+
+export { spotifyGreen }
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import React, { FC } from 'react'
+import { ClientSafeProvider, getProviders, LiteralUnion, signIn } from 'next-auth/react'
+import { BuiltInProviderType } from 'next-auth/providers'
+import clsx from 'clsx'
 
-const Login = () => {
-    return <div> login </div>
+interface Props {
+    providers: Promise<Record<LiteralUnion<BuiltInProviderType, string>, ClientSafeProvider>>
+}
+
+const Login: FC<Props> = ({ providers }) => {
+    return (
+        <div className="flex flex-col bg-black w-full min-h-screen items-center justify-center">
+            <img className="w-52 mb-5" src="/spotify-logo.svg" alt="Spotify Logo" />
+
+            {Object.values(providers).map(provider => (
+                <div key={provider.name}>
+                    <button
+                        className="bg-[#1DB954] text-white p-5 rounded-lg"
+                        onClick={() =>
+                            signIn(provider.id, {
+                                callbackUrl: '/'
+                            })
+                        }
+                    >
+                        Login with {provider.name}
+                    </button>
+                </div>
+            ))}
+        </div>
+    )
 }
 
 export default Login
+
+export async function getServerSideProps() {
+    const providers = await getProviders()
+    return {
+        props: {
+            providers
+        }
+    }
+}
